e34ad0e57c76fbdc56dd85da6a9baca02ca9b8f2
Marco <antonio_meza07@hotmail.com>
2020-05-16 02:41:37 -0500

    Add whatsapp web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'alcont';
+  chatView = false;
+
+  chat() {
+    this.chatView = !this.chatView;
+  }
+
+  sendMessage(numero) {
+    console.log(numero);
+    var link = "https://web.whatsapp.com/send?phone=" + numero + "+&text=Hola!%20Quiero%20contratar%20sus%20servicios!"    ;
+    console.log(link);
+    window.open(link, '_blank')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatMenuModule} from '@angular/material/menu';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +27,12 @@ import { ContactoComponent } from './contacto/contacto.component';
     BrowserModule,
     AppRoutingModule,
     MatMenuModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     CarouselModule,
     BrowserAnimationsModule // Browser Animation, Must for pop-up menu
   ],
