705fc863c95bbd859bc8332864701242642aa6b5
Marco <antonio_meza07@hotmail.com>
2020-05-26 19:47:52 -0500

    Add animation pages

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.scss']
+  styleUrls: ['./contacto.component.scss'],
+  animations: [
+      trigger('animationInOut', [
+          state('in',
+              style({ opacity: 1, transform: 'translateY(0)' })),
+          transition('void => *', [
+              style({ opacity: 0, transform: 'scale(1.05)' }),
+              animate('0.5s ease-in')
+          ])
+      ])
+  ]
 })
 export class ContactoComponent implements OnInit {
 
@@ -13,6 +24,6 @@ export class ContactoComponent implements OnInit {
   }
 
   sendMail (){
-    
+
   }
 }
